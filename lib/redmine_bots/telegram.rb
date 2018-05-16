5600f377b717f0ffc8c5a809b61777ec0f4621de
Vladislav Yashin <v.yashin.work@gmail.com>
2018-05-16 17:52:23 +0300

    Remove obsolete method

--- lib/redmine_bots/telegram.rb
+++ lib/redmine_bots/telegram.rb
@@ -14,23 +14,6 @@ module RedmineBots::Telegram
     Setting.find_by_name(:plugin_redmine_bots).value['telegram_bot_token']
   end
 
-  def self.bot_collisions
-    tokens = {}
-    if defined?(Intouch)
-      tokens[:redmine_intouch] = Setting.plugin_redmine_intouch['telegram_bot_token']
-    end
-    if defined?(RedmineChatTelegram)
-      tokens[:redmine_chat_telegram] = Setting.plugin_redmine_chat_telegram['bot_token']
-    end
-    if defined?(Redmine2FA)
-      tokens[:redmine_2fa] = Setting.plugin_redmine_2fa['bot_token']
-    end
-    tokens.delete_if { |_, v| v.nil? }
-    token_values = tokens.values
-    duplicates = token_values.uniq.map { |e| [token_values.count(e), e] }.select { |c, _| c > 1 }.map { |_, e| e }
-    duplicates.map { |token| tokens.select { |k, v| v == token }.keys }
-  end
-
   def self.update_manager
     @update_manager ||= UpdateManager.new
   end
