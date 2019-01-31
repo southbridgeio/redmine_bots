bd8c5f64fe48a2c48ef274b820caf8b8a6804945
Vladislav Yashin <v.yashin.work@gmail.com>
2019-01-31 18:53:40 +0400

    Fix inconsistent member kicks

--- lib/redmine_bots/telegram/tdlib/close_chat.rb
+++ lib/redmine_bots/telegram/tdlib/close_chat.rb
@@ -8,8 +8,11 @@ module RedmineBots::Telegram::Tdlib
         client.get_chat(chat_id).then do |chat|
           client.get_basic_group_full_info(chat.type.basic_group_id)
         end.flat.then do |group_info|
-          bot_members, regular_members = group_info.members.partition { |m| m.user_id.in?(robot_ids) }
-          member_ids = (regular_members + bot_members).map(&:user_id)
+          bot_id, robot_id = robot_ids
+          bot_member_ids, regular_member_ids = group_info.members.partition { |m| m.user_id.in?(robot_ids) }.map do |arr|
+            arr.map(&:user_id)
+          end
+          member_ids = (regular_member_ids + (bot_member_ids & [bot_id]) + (bot_member_ids & [robot_id]))
           member_ids.reduce(Promises.fulfilled_future(nil)) do |promise, member_id|
             promise.then { delete_member(chat_id, member_id) }.flat
           end
