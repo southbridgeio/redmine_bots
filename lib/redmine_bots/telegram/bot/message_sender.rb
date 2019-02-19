09c8480f7e3f61e84207c72ef3470f020ff40272
Vladislav Yashin <v.yashin.work@gmail.com>
2019-02-19 22:04:50 +0400

    Handle socks connection errors

--- lib/redmine_bots/telegram/bot/message_sender.rb
+++ lib/redmine_bots/telegram/bot/message_sender.rb
@@ -22,6 +22,12 @@ module RedmineBots::Telegram
         end
       end
 
+      class SocksConnectionError
+        def self.===(e)
+          e.is_a?(Faraday::ConnectionFailed) && e.message.include?("Can't complete SOCKS5 connection to")
+        end
+      end
+
       def self.call(params)
         new(params).call
       end
@@ -37,6 +43,7 @@ module RedmineBots::Telegram
 
       def call
         tries ||= 3
+        socks_tries ||= 50
         message_params = {
           chat_id: chat_id,
           text: message,
@@ -53,6 +60,8 @@ module RedmineBots::Telegram
         logger.warn("Too many requests. Sleeping #{SLEEP_TIME} seconds...")
         sleep SLEEP_TIME
         (tries -= 1).zero? ? raise(e) : retry
+      rescue SocksConnectionError => e
+        (socks_tries -= 1).zero? ? raise(e) : retry
       rescue Faraday::ClientError => e
         logger.warn("Faraday client error. Sleeping #{FARADAY_SLEEP_TIME} seconds...")
         sleep FARADAY_SLEEP_TIME
