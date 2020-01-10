455ae4c7d3d1486b72cde7274228d55f966a4dd9
Vladislav Yashin <v.yashin.work@gmail.com>
2020-01-10 13:28:32 +0400

    Fix chat creation according to tdlib 1.6

--- lib/redmine_bots/telegram/tdlib/create_chat.rb
+++ lib/redmine_bots/telegram/tdlib/create_chat.rb
@@ -3,9 +3,22 @@ module RedmineBots::Telegram::Tdlib
     def call(title, user_ids)
       Promises.zip(*user_ids.map(&client.method(:get_user))).then do
         client.create_new_basic_group_chat(user_ids, title).then do |chat|
-          client.toggle_basic_group_administrators(chat.type.basic_group_id, false).then { chat }
+          client.set_chat_permissions(chat.id, permissions).then { chat }
         end.flat
       end.flat
+    end
+
+    private
+
+    def permissions
+      ChatPermissions.new(can_send_messages: true,
+                          can_send_media_messages: true,
+                          can_send_polls: true,
+                          can_send_other_messages: true,
+                          can_add_web_page_previews: true,
+                          can_change_info: false,
+                          can_invite_users: false,
+                          can_pin_messages: false)
     end
   end
 end
