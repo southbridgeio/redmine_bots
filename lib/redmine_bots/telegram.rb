20da6a4fe0cab3c357eb6a808de4c9e273461880
Vladislav Yashin <v.yashin.work@gmail.com>
2020-01-13 18:20:39 +0400

    Fix AddBot command

--- lib/redmine_bots/telegram.rb
+++ lib/redmine_bots/telegram.rb
@@ -44,15 +44,7 @@ module RedmineBots::Telegram
     bot_info = bot.api.get_me['result']
     bot_name = bot_info['username']
 
-    until bot_name.present?
-      sleep 60
-
-      bot      = Telegram::Bot::Client.new(token)
-      bot_info = bot.api.get_me['result']
-      bot_name = bot_info['username']
-
-      RedmineBots::Telegram::Tdlib::AddBot.(bot_name) if robot_id
-    end
+    RedmineBots::Telegram::Tdlib::AddBot.(bot_name) if robot_id
 
     plugin_settings = Setting.find_by(name: 'plugin_redmine_bots')
 
