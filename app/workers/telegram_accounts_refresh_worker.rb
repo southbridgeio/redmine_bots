6458813e9c76ca1700d9a6331ac2e59e2a24ac97
Vladislav Yashin <v.yashin.work@gmail.com>
2020-12-23 10:07:06 +0400

    Update TelegramAccountsRefreshWorker

--- app/workers/telegram_accounts_refresh_worker.rb
+++ app/workers/telegram_accounts_refresh_worker.rb
@@ -7,6 +7,7 @@ class TelegramAccountsRefreshWorker
     TelegramAccount.where.not(telegram_id: nil).find_each do |account|
       new_data = RedmineBots::Telegram.bot.get_chat(chat_id: account.telegram_id)
       account.update(**new_data['result'].slice('username', 'first_name', 'last_name').symbolize_keys)
+      sleep 1
     end
   end
 end
