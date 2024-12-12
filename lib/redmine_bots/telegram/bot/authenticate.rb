9d6196640c786dd5c452d7620c622440b8a0c32f
nevrfl <mvvolkov96@gmail.com>
2024-12-12 13:05:19 +0300

    1072560 Fix TG account connection through 2FA

--- lib/redmine_bots/telegram/bot/authenticate.rb
+++ lib/redmine_bots/telegram/bot/authenticate.rb
@@ -3,6 +3,8 @@ module RedmineBots
     class Bot::Authenticate
       AUTH_TIMEOUT = 60.minutes
 
+      attr_reader :login_error
+
       def self.call(user, auth_data, context:)
         new(user, auth_data, context: context).call
       end
@@ -19,27 +21,31 @@ module RedmineBots
         case @context
         when '2fa_connection'
           telegram_account = prepare_telegram_account(model_class: RedmineTwoFa::TelegramConnection)
-          return failure(I18n.t('redmine_bots.telegram.bot.login.errors.wrong_account')) unless telegram_account
+          return failure(login_error) unless telegram_account
         when 'account_connection'
           telegram_account = prepare_telegram_account(model_class: TelegramAccount)
-          return failure(I18n.t('redmine_bots.telegram.bot.login.errors.wrong_account')) unless telegram_account
+          return failure(login_error) unless telegram_account
           telegram_account.assign_attributes(@auth_data.slice('first_name', 'last_name', 'username'))
         else
           return failure('Invalid context')
         end
 
         TelegramAccount.transaction do
-          next failure(I18n.t('redmine_bots.telegram.bot.login.errors.not_persisted')) unless telegram_account.save
+          return failure(I18n.t('redmine_bots.telegram.bot.login.errors.not_persisted')) unless telegram_account.save
 
-          next success(telegram_account) if create_account_after_2fa(telegram_account)
-
-          failure(I18n.t('redmine_bots.telegram.bot.login.errors.not_persisted'))
+          if create_account_after_2fa(telegram_account)
+            return success(telegram_account)
+          end
 
           raise ActiveRecord::Rollback
         end
+
+        failure(login_error)
       end
 
       private
+
+      attr_writer :login_error
 
       def prepare_telegram_account(model_class:)
         telegram_account = model_class.find_by(user_id: @user.id)
@@ -47,6 +53,7 @@ module RedmineBots
         if telegram_account.present?
           if telegram_account.telegram_id
             if @auth_data['id'].to_i != telegram_account.telegram_id
+              self.login_error = I18n.t('redmine_bots.telegram.bot.login.errors.wrong_account')
               return nil
             end
           else
@@ -56,6 +63,7 @@ module RedmineBots
           telegram_account = model_class.find_or_initialize_by(telegram_id: @auth_data['id'])
           if telegram_account.user_id
             if telegram_account.user_id != @user.id
+              self.login_error = (I18n.t('redmine_bots.telegram.bot.login.errors.account_exists'))
               return nil
             end
           else
@@ -70,7 +78,7 @@ module RedmineBots
 
         telegram_account_2fa = prepare_telegram_account(model_class: TelegramAccount)
 
-        return false unless telegram_account_2fa
+        return false if telegram_account_2fa.blank?
 
         telegram_account_2fa.assign_attributes(@auth_data.slice('first_name', 'last_name', 'username'))
 
